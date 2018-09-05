4301c9ad36666262c0e0da804199fed068a1d820
dillon <dillon@dickersonsoftware.com>
2018-09-04 23:26:32 -0700

    initial commit

---
+++ build/service/src/com/dickersonsoftware/intuit/ExtensionMapper.java
@@ -0,0 +1,28 @@
+/**
+ * ExtensionMapper.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.7.8  Built on : May 19, 2018 (07:06:38 BST)
+ */
+package com.dickersonsoftware.intuit;
+
+
+/**
+ *  ExtensionMapper class
+ */
+@SuppressWarnings({"unchecked",
+    "unused"
+})
+public class ExtensionMapper {
+    public static java.lang.Object getTypeObject(
+        java.lang.String namespaceURI, java.lang.String typeName,
+        javax.xml.stream.XMLStreamReader reader) throws java.lang.Exception {
+        if ("http://intuit.dickersonsoftware.com/".equals(namespaceURI) &&
+                "ArrayOfString".equals(typeName)) {
+            return com.dickersonsoftware.intuit.ArrayOfString.Factory.parse(reader);
+        }
+
+        throw new org.apache.axis2.databinding.ADBException("Unsupported type " +
+            namespaceURI + " " + typeName);
+    }
+}
