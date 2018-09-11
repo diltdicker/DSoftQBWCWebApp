0fb87c790ff683d5f72891e97ec9422527079c79
dillon <dillon@dickersonsoftware.com>
2018-09-11 10:58:11 -0700

    response lookup

--- DSoftQBWCWebApp/src/com/dsoft/qbwcwebapp/api/Response.java
+++ DSoftQBWCWebApp/src/com/dsoft/qbwcwebapp/api/Response.java
@@ -11,6 +11,14 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response.Status;
 
+import org.bson.Document;
+
+import com.dsoft.qbwcwebapp.db.DBProxyFactory;
+import com.dsoft.qbwcwebapp.model.Account;
+
+import net.sf.json.JSON;
+import net.sf.json.xml.XMLSerializer;
+
 /**
  * @author dillon
  *
@@ -19,14 +27,31 @@ import javax.ws.rs.core.Response.Status;
 public class Response {
 
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public javax.ws.rs.core.Response getResponse(@PathParam("username") String username, @PathParam("reqid") String reqIDStr) {
-		
-		return javax.ws.rs.core.Response.ok(username + " a test").status(Status.OK).build();
+	public javax.ws.rs.core.Response getResponse(@PathParam("username") String username, @PathParam("reqid") Long reqIDStr) {
+		Document document = DBProxyFactory.getFactory().getAccounts().getDocument(new Document().append("username", username));
+		if (document != null) {
+			Account account = new Account(document);
+			document = DBProxyFactory.getFactory().getResponses().getDocument(new Document().append("ticket", account.getTicket()).append("reqID", reqIDStr));
+			if (document != null) {
+				com.dsoft.qbwcwebapp.model.Response response = new com.dsoft.qbwcwebapp.model.Response(document);
+				XMLSerializer serializer = new XMLSerializer();
+				String json = ((JSON) serializer.read(response.getResponse())).toString();
+				return javax.ws.rs.core.Response.ok(json, MediaType.APPLICATION_JSON).status(Status.OK).build();
+			} else {
+				return javax.ws.rs.core.Response.noContent().status(Status.NOT_FOUND).build();
+			}
+		} else {
+			return javax.ws.rs.core.Response.noContent().status(Status.NOT_FOUND).build();
+		}
 	}
 	
 	@DELETE
-	public javax.ws.rs.core.Response deleteResponse(@PathParam("username") String username, @PathParam("reqid") String reqIDStr) {
+	public javax.ws.rs.core.Response deleteResponse(@PathParam("username") String username, @PathParam("reqid") Long reqIDStr) {
+		Document document = DBProxyFactory.getFactory().getAccounts().getDocument(new Document().append("username", username));
+		if (document != null) {
+			Account account = new Account(document);
+			DBProxyFactory.getFactory().getResponses().deleteDocument(new Document().append("ticket", account.getTicket()).append("reqID", reqIDStr));
+		}
 		return javax.ws.rs.core.Response.noContent().status(Status.OK).build();
 	}
 }
