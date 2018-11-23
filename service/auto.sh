8333354406facc153564f15503895cb2b138a642
Dillon Dickerson a.k.a. (Dillion) <diltdicker@gmail.com>
2018-11-23 13:33:07 -0700

    Delete auto.sh

--- service/auto.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-ant jar.server
-mv /home/dillon/Projects/DSoftQBWCSoapService/service/build/lib/DSoftQBWCSoapService.aar /home/dillon/apache-tomcat-9.0.11.8081/webapps/axis2/WEB-INF/services/
-ant clean
